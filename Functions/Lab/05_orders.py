0f9af413f18917b6b0c7cc67b2ccf47cc5a8c8f4
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-02-04 14:18:16 +0200

    Add to Functions

---
+++ Functions/Lab/05_orders.py
@@ -0,0 +1,18 @@
+def calculate_price(order, n):
+    total_price = 0
+    if order == "coffee":
+        total_price = n * 1.50
+    elif order == "water":
+        total_price = n * 1
+    elif order == "coke":
+        total_price = n * 1.40
+    elif order == "snacks":
+        total_price = n * 2
+    return total_price
+
+
+product = input()
+quantity = int(input())
+
+print(f"{calculate_price(product, quantity):.2f}")
+
