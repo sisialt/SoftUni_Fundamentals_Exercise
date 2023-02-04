0945ef6e36e29d0643bae2a5eeaec9d860ed5e4c
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-02-04 17:55:38 +0200

    Add to Functions

---
+++ Functions/01_smallest_of_three_numbers.py
@@ -0,0 +1,9 @@
+def find_smallest(n1, n2, n3):
+    return min(n1, n2, n3)
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(find_smallest(num1, num2, num3))
---
+++ Functions/02_add_and_subtract.py
@@ -0,0 +1,21 @@
+def sum_numbers(n1, n2):
+    sum_num = n1 + n2
+    return sum_num
+
+
+def subtract(sum1_2, n3):
+    subtract_sum_n3 = sum1_2 - n3
+    return subtract_sum_n3
+
+
+def add_and_subtract(n1, n2, n3):
+    sum_numbers(n1, n2)
+    add_and_subt = subtract(sum_numbers(n1, n2), n3)
+    return add_and_subt
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(subtract(sum_numbers(num1, num2), num3))
