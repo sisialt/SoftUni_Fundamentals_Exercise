0f2a5fbca1cb5e935d2e83714e9d2ad23daeb13d
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-02-10 10:35:56 +0200

    list adv

--- List advanced/06_electron_distribution.py
+++ List advanced/06_electron_distribution.py
@@ -1,16 +1,16 @@
 electrons = int(input())
 
-shells = []
+shells_filled = []
 left_electrons = electrons
 
 for n in range(1, electrons):
     max_electrons = 2 * (n ** 2)
     left_electrons -= max_electrons
-    shells.append(max_electrons)
+    shells_filled.append(max_electrons)
 
     if left_electrons <= 2 * ((n + 1) ** 2):
-        shells.append(left_electrons)
+        shells_filled.append(left_electrons)
         break
 
-print(shells)
+print(shells_filled)
 
