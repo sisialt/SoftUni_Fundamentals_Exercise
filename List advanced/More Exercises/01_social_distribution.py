af6c6326f2693dff3f36eee02f7fa71edc352cf6
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-02-11 22:44:09 +0200

    list adv more

---
+++ List advanced/More Exercises/01_social_distribution.py
@@ -0,0 +1,20 @@
+population = [int(x) for x in input().split(", ")]
+minimum_wealth = int(input())
+
+new_population = population.copy()
+total_difference_wealth_to_minimum = 0
+
+for poor in population:
+    if poor < minimum_wealth:
+        total_difference_wealth_to_minimum += minimum_wealth - poor
+        new_population.remove(poor)
+        new_population.insert(population.index(poor), minimum_wealth)
+
+wealthiest = max(population)
+
+if wealthiest - total_difference_wealth_to_minimum >= minimum_wealth:
+    new_population.insert(population.index(wealthiest), wealthiest - total_difference_wealth_to_minimum)
+    new_population.remove(wealthiest)
+    print(new_population)
+else:
+    print("No equal distribution possible")
