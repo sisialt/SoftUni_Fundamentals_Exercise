19fe1789c2e445e6cbb4da76dbc4f1ae458ada5a
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-02-13 19:19:42 +0200

    up

--- Mid_exam_preparation/01_bonus_scoring_system.py
+++ Mid_exam_preparation/01_bonus_scoring_system.py
@@ -4,15 +4,20 @@ students = int(input())
 lectures = int(input())
 bonus = int(input())
 
-max_bonus = float("-inf")
+max_bonus = 0
 attendances_for_max_student = 0
+
 
 for _ in range(students):
     attendances = int(input())
-    total_bonus = attendances / lectures * (5 + bonus)
+    if lectures != 0:
+        total_bonus = attendances / lectures * (5 + bonus)
 
-    if total_bonus > max_bonus:
-        max_bonus = total_bonus
-        attendances_for_max_student = attendances
+        if total_bonus > max_bonus:
+            max_bonus = total_bonus
+            attendances_for_max_student = attendances
 
-print(f"Max Bonus: {round(max_bonus)}.\nThe student has attended {attendances_for_max_student} lectures.")
+print(f"Max Bonus: {ceil(max_bonus)}.\nThe student has attended {attendances_for_max_student} lectures.")
+
+# not working with max_bonus = float("-inf")
+# works without line 13 - bug in judge
