9a5f54c5a20e2ea3aafe2a49e93a75dc8ba192ed
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-02 21:48:10 +0200

    class 03

---
+++ Class and objects/Lab/03_email.py
@@ -0,0 +1,29 @@
+class Email:
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+        self.is_sent = False
+
+    def send(self):
+        self.is_sent = True
+
+    def get_info(self):
+        return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
+
+
+emails = []
+data_input = input()
+
+while data_input != "Stop":
+    data_input = data_input.split()
+    email = Email(data_input[0], data_input[1], data_input[2])
+    emails.append(email)
+    data_input = input()
+
+indexes = [int(x) for x in input().split(", ")]
+
+for i, email in enumerate(emails):
+    if i in indexes:
+        emails[i].send()
+    print(email.get_info())
