a2ca6008dd72ab1d95ca96f3cdd6b359cf399962
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-05 18:27:45 +0200

    dict 10

---
+++ Dictionaries/10_company_users.py
@@ -0,0 +1,19 @@
+input_data = input()
+
+company_users = {}
+
+while input_data != "End":
+    company, employee = input_data.split(" -> ")
+
+    if company not in company_users:
+        company_users[company] = []
+
+    if employee not in company_users[company]:
+        company_users[company].append(employee)
+
+    input_data = input()
+
+for company_name in company_users:
+    print(company_name)
+    for employee_id in range(len(company_users[company_name])):
+        print(f"-- {company_users[company_name][employee_id]}")
