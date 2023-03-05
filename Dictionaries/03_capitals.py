e90a0cba64f60c12d27af7fe78929a5ec587a2d3
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-05 18:26:55 +0200

    dict 03

---
+++ Dictionaries/03_capitals.py
@@ -0,0 +1,7 @@
+countries = input().split(", ")
+capitals = input().split(", ")
+
+result = dict(zip(countries, capitals))
+
+for country, capital in result.items():
+    print(f"{country} -> {capital}")
