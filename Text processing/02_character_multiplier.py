384fa2ecb711f3242d209a7249ad148a440d4f6f
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-11 23:07:35 +0200

    up

--- Text processing/02_character_multiplier.py
+++ Text processing/02_character_multiplier.py
@@ -12,3 +12,9 @@ for i in range(max(len(string1), len(string2))):
             result += ord(string2[i])
 
 print(result)
+
+# for i in range(min(len(string1), len(string2))):
+#     result += ord(string1[i]) * ord(string2[i])
+#
+# result += sum([ord(char) for char in string1[min(len(string1), len(string2)):]])
+# result += sum([ord(char) for char in string2[min(len(string1), len(string2)):]])
