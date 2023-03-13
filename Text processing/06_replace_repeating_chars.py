ea39133d423ffe84759fb42949fe6b2cde2bc4ba
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-13 21:39:09 +0200

    up

--- Text processing/06_replace_repeating_chars.py
+++ Text processing/06_replace_repeating_chars.py
@@ -3,7 +3,7 @@ string_without_repeating_chars = ""
 
 for i, ch in enumerate(string):
     if i != len(string) - 1 and ch == string[i + 1]:
-        string_without_repeating_chars += ch.replace(ch, "")
+        string_without_repeating_chars += ""
         i += 1
     else:
         string_without_repeating_chars += ch
