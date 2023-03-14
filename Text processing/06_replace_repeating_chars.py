27ae4cf1fe82b693d01f0a3322e7b4725a8cb559
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-14 12:30:10 +0200

    up

--- Text processing/06_replace_repeating_chars.py
+++ Text processing/06_replace_repeating_chars.py
@@ -1,11 +1,20 @@
 string = input()
-string_without_repeating_chars = ""
+# string_without_repeating_chars = ""
+#
+# # for i, ch in enumerate(string):
+#     if i != len(string) - 1 and ch == string[i + 1]:
+#         string_without_repeating_chars += ""    # continue
+#         i += 1
+#     else:
+#         string_without_repeating_chars += ch
+#
+# print(string_without_repeating_chars)
 
-for i, ch in enumerate(string):
-    if i != len(string) - 1 and ch == string[i + 1]:
-        string_without_repeating_chars += ""
-        i += 1
-    else:
-        string_without_repeating_chars += ch
+string_without_repeating_chars = string[0]
+
+for ch in string:
+    if ch == string_without_repeating_chars[-1]:
+        continue
+    string_without_repeating_chars += ch
 
 print(string_without_repeating_chars)
