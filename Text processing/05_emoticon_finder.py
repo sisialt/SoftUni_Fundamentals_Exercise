94c8924decf5b04aac4215ee1dd57f5c074baec2
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-14 12:30:05 +0200

    up

--- Text processing/05_emoticon_finder.py
+++ Text processing/05_emoticon_finder.py
@@ -2,6 +2,7 @@ string = input()
 emoticon = ""
 
 for i, ch in enumerate(string):
-    if ch == ":":
+    if ch == ":" and i + 1 < len(string):
         emoticon = ch + string[i + 1]
+        i += 1
         print(emoticon)
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-string_input = input()
-new_string = ""
-result = ""
-
-while len(string_input):
-    for i in range(len(string_input)):
-        if string_input[i] == ">":
-            result = string_input[:i + 1]
-            break
-    else:
-        result = string_input
-
-    string_input = string_input.replace(result, "")
-    new_string += result
-
-    if string_input:
-        if string_input[0].isdigit():
-            explosion = int(string_input[0])
-            for n in range(explosion):
-                string_input = string_input.replace(string_input[0], "", 1)
-        else:
-            new_string += string_input
-            string_input = ""
-
-print(new_string)
-print(string_input)
-
-
+# string_input = input()
+# new_string = ""
+# result = ""
+#
+# while len(string_input):
+#     for i in range(len(string_input)):
+#         if string_input[i] == ">":
+#             result = string_input[:i + 1]
+#             break
+#     else:
+#         result = string_input
+#
+#     string_input = string_input.replace(result, "")
+#     new_string += result
+#
+#     if string_input:
+#         if string_input[0].isdigit():
+#             explosion = int(string_input[0])
+#             for n in range(explosion):
+#                 string_input = string_input.replace(string_input[0], "", 1)
+#         else:
+#             new_string += string_input
+#             string_input = ""
+#
+# print(new_string)
+# print(string_input)
+#
+#
+text = "ab12"
+for i in range(len(text)):
+    if int(text[i]):
+        print("hi")
