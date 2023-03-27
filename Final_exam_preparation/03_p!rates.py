75e0350683b67ea72824c04ab999660731548e87
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-27 21:42:47 +0300

    up

--- Final_exam_preparation/03_p!rates.py
+++ Final_exam_preparation/03_p!rates.py
@@ -1,4 +1,4 @@
-pirates = {}
+cities = {}
 
 while True:
     line = input()
@@ -10,11 +10,11 @@ while True:
     population = int(line_args[1])
     gold = int(line_args[2])
 
-    if city not in pirates:
-        pirates[city] = [population, gold]
+    if city not in cities:
+        cities[city] = {"population": population, "gold": gold}
     else:
-        pirates[city][0] += population
-        pirates[city][1] += gold
+        cities[city]["population"] += population
+        cities[city]["gold"] += gold
 
 while True:
     line = input()
@@ -28,13 +28,13 @@ while True:
         people = int(line_args[2])
         gold_stolen = int(line_args[3])
 
-        pirates[town][0] -= people
-        pirates[town][1] -= gold_stolen
+        cities[town]["population"] -= people
+        cities[town]["gold"] -= gold_stolen
 
         print(f"{town} plundered! {gold_stolen} gold stolen, {people} citizens killed.")
 
-        if pirates[town][0] <= 0 or pirates[town][1] <= 0:
-            pirates.pop(town)
+        if cities[town]["population"] <= 0 or cities[town]["gold"] <= 0:
+            cities.pop(town)
             print(f"{town} has been wiped off the map!")
 
     elif line_args[0] == "Prosper":
@@ -43,14 +43,59 @@ while True:
             print("Gold added cannot be a negative number!")
             continue
 
-        pirates[town][1] += gold_added
+        cities[town]["gold"] += gold_added
 
-        print(f"{gold_added} gold added to the city treasury. {town} now has {pirates[town][1]} gold.")
+        print(f"{gold_added} gold added to the city treasury. {town} now has {cities[town]['gold']} gold.")
 
-if pirates:
-    print(f"Ahoy, Captain! There are {len(pirates)} wealthy settlements to go to:")
-    for key_town, value_people_gold in pirates.items():
-        print(f"{key_town} -> Population: {value_people_gold[0]} citizens, Gold: {value_people_gold[1]} kg")
+if cities:
+    print(f"Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:")
+    for key_town, value_people_gold in cities.items():
+        print(f"{key_town} -> Population: {value_people_gold['population']} citizens, Gold: {value_people_gold['gold']} kg")
 
 else:
     print("Ahoy, Captain! All targets have been plundered and destroyed!")
+
+#     if city not in cities:
+#         cities[city] = [population, gold]  # cities[city] = {"population": population, "gold": gold}
+#     else:
+#         cities[city][0] += population  # cities[city]["population"] += population
+#         cities[city][1] += gold  # -> ["gold"]
+#
+# while True:
+#     line = input()
+#     if line == "End":
+#         break
+#
+#     line_args = line.split("=>")
+#     town = line_args[1]
+#
+#     if line_args[0] == "Plunder":
+#         people = int(line_args[2])
+#         gold_stolen = int(line_args[3])
+#
+#         cities[town][0] -= people  # cities[town]["population"] -= people
+#         cities[town][1] -= gold_stolen  # -> ["gold"]
+#
+#         print(f"{town} plundered! {gold_stolen} gold stolen, {people} citizens killed.")
+#
+#         if cities[town][0] <= 0 or cities[town][1] <= 0:
+#             cities.pop(town)
+#             print(f"{town} has been wiped off the map!")
+#
+#     elif line_args[0] == "Prosper":
+#         gold_added = int(line_args[2])
+#         if gold_added < 0:
+#             print("Gold added cannot be a negative number!")
+#             continue
+#
+#         cities[town][1] += gold_added
+#
+#         print(f"{gold_added} gold added to the city treasury. {town} now has {cities[town][1]} gold.")
+#
+# if cities:
+#     print(f"Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:")
+#     for key_town, value_people_gold in cities.items():
+#         print(f"{key_town} -> Population: {value_people_gold[0]} citizens, Gold: {value_people_gold[1]} kg")
+#
+# else:
+#     print("Ahoy, Captain! All targets have been plundered and destroyed!")
