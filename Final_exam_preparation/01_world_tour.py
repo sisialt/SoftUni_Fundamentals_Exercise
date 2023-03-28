eedd40caf6ff2a0a159e0322d9d447fe12e7645d
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-03-28 11:30:25 +0300

    up

--- Final_exam_preparation/01_world_tour.py
+++ Final_exam_preparation/01_world_tour.py
@@ -1,5 +1,5 @@
 def is_valid_index(idx, text):
-    if 0 <= idx < len(text):
+    if 0 <= idx < len(text):  # return 0 <= idx < len(text)
         return True
     return False
 
@@ -20,7 +20,6 @@ while True:
 
         if is_valid_index(index, input_string):
             input_string = input_string[:index] + string_to_add + input_string[index:]
-        print(input_string)
 
     elif action == "Remove Stop":
         start_index = int(command_args[1])
@@ -28,14 +27,14 @@ while True:
 
         if is_valid_index(start_index, input_string) and is_valid_index(end_index, input_string):
             input_string = input_string[:start_index] + input_string[end_index + 1:]
-        print(input_string)
 
     elif action == "Switch":
         old_string = command_args[1]
         new_string = command_args[2]
 
-        if old_string in input_string:
+        if old_string in input_string:  # without if, .replace() doesnt do anything when no old string
             input_string = input_string.replace(old_string, new_string)
-        print(input_string)
+
+    print(input_string)
 
 print(f"Ready for world tour! Planned stops: {input_string}")
