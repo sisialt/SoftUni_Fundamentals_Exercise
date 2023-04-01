d9eced4c4529a365c3b0335d657374e743e52184
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-04-01 22:55:31 +0300

    Create 02_take_skip_rope

--- List advanced/More Exercises/02_take_skip_rope.py
+++ List advanced/More Exercises/02_take_skip_rope.py
@@ -1,1 +1,22 @@
-string = input()
+text = input()
+
+digits = []
+chars = ""
+result = ""
+
+for ch in text:
+    if ch.isdigit():
+        digits.append(int(ch))
+    else:
+        chars += ch
+
+take_list = [digits[i] for i in range(len(digits)) if i % 2 == 0]
+skip_list = [digits[i] for i in range(len(digits)) if i % 2 != 0]
+
+for i in range(len(take_list)):
+    take = take_list[i]
+    skip = skip_list[i]
+    result += chars[:take]
+    chars = chars[take + skip:]
+
+print(result)
