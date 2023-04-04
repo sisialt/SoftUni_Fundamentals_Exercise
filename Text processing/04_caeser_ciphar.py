dc671966abd98cb13e8194bdfaaafd6ef75ad5ff
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-04-04 21:35:04 +0300

    Move files

--- Text processing/04_caeser_ciphar.py
+++
@@ -1,7 +0,0 @@
-text = input()
-encrypted_text = ""
-
-for ch in text:
-    encrypted_text += chr(ord(ch) + 3)
-
-print(encrypted_text)
--- Text processing/05_emoticon_finder.py
+++
@@ -1,8 +0,0 @@
-string = input()
-emoticon = ""
-
-for i, ch in enumerate(string):
-    if ch == ":" and i + 1 < len(string):
-        emoticon = ch + string[i + 1]
-        i += 1
-        print(emoticon)
