b9b286e7a884bcbd2f2736488cad52a53824e4c6
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-04-06 13:42:38 +0300

    Update 01_reverse_strings

--- Text processing/Lab/01_reverse_strings.py
+++ Text processing/Lab/01_reverse_strings.py
@@ -1,6 +1,6 @@
-word = input()
-
-while word != "end":
-    print(f"{word} = {word[::-1]}")
-
+while True:
     word = input()
+    if word != "end":
+        break
+        
+    print(f"{word} = {word[::-1]}")
