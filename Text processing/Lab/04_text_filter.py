304d378241e3680d12377b350ce355254ae14106
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-04-06 13:43:04 +0300

    Update 04_text_filter

--- Text processing/Lab/04_text_filter.py
+++ Text processing/Lab/04_text_filter.py
@@ -2,6 +2,7 @@ banned_words = input().split(", ")
 text = input()
 
 for word in banned_words:
+
     while word in text:
         length = len(word)
         text = text.replace(word, "*" * length)
